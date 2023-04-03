f28f70133a671e118028540df30b23d3de3a5931
xueyanli <xueyanli19950914@gmail.com>
2023-04-03 19:41:17 +0800

    ci debug over

--- test/helper/Sshd.py
+++ test/helper/Sshd.py
@@ -23,7 +23,7 @@ class Sshd:
             return None
 
     @staticmethod
-    @timeout(90)
+    @timeout(120)
     async def execute(cmd: str, args: List[str] = None) -> str:
         """
         执行命令行，返回命令行输出结果

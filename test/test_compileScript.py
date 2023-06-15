f5670ff49c018ae6914a3580dd503d418914b3a2
xueyanli <xueyanli19950914@gmail.com>
2023-06-15 13:18:17 +0800

    add blake160 by Makefile

--- test/test_compileScript.py
+++ test/test_compileScript.py
@@ -22,23 +22,30 @@ class TestCompileScript:
     ])
     def test_generateScriptBinByToolChain(self, toolchain, compiletype):
         container_name = generate_random_string(5)
-        script_bin = generate_random_string(3) + "-" + compiletype + "Bin"
         scripts_dir = os.path.abspath(os.path.join(os.getcwd(), "data/scripts"))
-        os.environ['Bin'] = script_bin
-        try:
-            self.loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self.loop)
-            asyncio.run(
-                Sshd.execute("docker", ["run", "-itd", "--name", f"{container_name}", "-v", f"{scripts_dir}:/code",
-                                        f"{toolchain}", "bash"]))
-            time.sleep(30)
-            asyncio.run(Sshd.executeByString(
-                f"docker exec -itd {container_name} sh -c ' cd code/ && {compiletype}  -Os {script_name} "
-                f"-o {script_bin}'"
-            ))
-        except RuntimeError as e:
-            logger.error(f"Error run: {e}")
-        finally:
-            self.loop.close()
-            Sshd.execute_sync("make install")
-            Sshd.execute_sync("make ci")
+        # 获取目标目录下的所有文件名
+        file_names = []
+        for filename in os.listdir(scripts_dir):
+            if filename.endswith(".c"):
+                file_names.append(filename)
+        for script_name in file_names:
+            script_bin = script_name + "-" + compiletype + "Bin"
+            os.environ['Bin'] = script_bin
+
+            try:
+                self.loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(self.loop)
+                asyncio.run(
+                    Sshd.execute("docker", ["run", "-itd", "--name", f"{container_name}", "-v", f"{scripts_dir}:/code",
+                                            f"{toolchain}", "bash"]))
+                time.sleep(30)
+                asyncio.run(Sshd.executeByString(
+                    f"docker exec -itd {container_name} sh -c ' cd code/ && {compiletype}  -Os {script_name} "
+                    f"-o {script_bin}'"
+                ))
+            except RuntimeError as e:
+                logger.error(f"Error run: {e}")
+            finally:
+                self.loop.close()
+                Sshd.execute_sync("make install")
+                Sshd.execute_sync("make ci")
